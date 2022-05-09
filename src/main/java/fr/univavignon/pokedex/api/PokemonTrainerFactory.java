00dd01fa79c538fd681f846a0ad895e785272f83
meghellet <said199811@live.fr>
2022-05-09 10:34:56 +0200

    Tp4 Épisode 3 - Make it work Implimentation de l'interface IPokemonTrainerFactory

---
+++ src/main/java/fr/univavignon/pokedex/api/PokemonTrainerFactory.java
@@ -0,0 +1,12 @@
+package fr.univavignon.pokedex.api;
+
+public class PokemonTrainerFactory implements IPokemonTrainerFactory{
+    @Override
+    public PokemonTrainer createTrainer(String name, Team team, IPokedexFactory pokedexFactory) {
+
+        IPokedex pokedex = pokedexFactory.createPokedex(new PokemonMetadataProvider(), new PokemonFactory());
+
+        return new PokemonTrainer(name, team, pokedex);
+        //return null;
+    }
+}
