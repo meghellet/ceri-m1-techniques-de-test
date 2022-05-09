8ea8554a82a433924d7f1124667fe120073f7023
meghellet <said199811@live.fr>
2022-05-09 09:36:41 +0200

    Tp4 Épisode 3 - Make it work Implimentation de l'interface IPokedex

---
+++ src/main/java/fr/univavignon/pokedex/api/Pokedex.java
@@ -0,0 +1,65 @@
+package fr.univavignon.pokedex.api;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Pokedex implements IPokedex{
+
+    ArrayList<Pokemon> listPokemons;
+    IPokemonMetadataProvider metadataProvider = null;
+    IPokemonFactory pokemonFactory = null;
+
+
+    public Pokedex(IPokemonMetadataProvider metadataProvider, IPokemonFactory pokemonFactory) {
+        this.listPokemons = new ArrayList<>();
+        this.metadataProvider = metadataProvider;
+        this.pokemonFactory = pokemonFactory;
+    }
+
+    @Override
+    public int size() {
+        return listPokemons.size();
+        //return 0;
+    }
+
+    @Override
+    public int addPokemon(Pokemon pokemon) {
+        listPokemons.add(pokemon);
+        int newLitsPockemons = listPokemons.size() - 1 ;
+        return newLitsPockemons ;
+        //return 0;
+    }
+
+    @Override
+    public Pokemon getPokemon(int id) throws PokedexException {
+        if(id > 150 || id < 0){
+            throw new PokedexException("L'id n'est pas compris entre 0 et 150 !");
+        }
+        Pokemon SearchPockemon = listPokemons.get(id);
+        return SearchPockemon ;
+    }
+
+    public List<Pokemon> getPokemons(){
+
+        return listPokemons;
+    }
+
+    @Override
+    public List<Pokemon> getPokemons(Comparator<Pokemon> order) {
+        // TODO Auto-generated method stub
+        List<Pokemon> listTrierByOrder = new ArrayList<>(this.listPokemons);
+        listTrierByOrder.sort(order);
+        return listTrierByOrder;
+
+        //return null;
+    }
+
+    @Override
+    public Pokemon createPokemon(int index, int cp, int hp, int dust, int candy) {
+        return  pokemonFactory.createPokemon(index, cp, hp, dust, candy);
+        //return null;
+    }
+
+
+}
